ef14cda9b838cee9cc92ed435a88030762aab4b8
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2025-10-09 17:15:16 +0200

    style(nav): add animation to nav
    add framer motion animation to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,63 @@
+"use client";
 import Link from "next/link"
+import { motion } from 'framer-motion';
 
 const Nav = () => {
-  return (
-    <nav className="hidden lg:block">
-        <ul className="mt-16 w-max">
-            <li>
-                <Link href='#about' className="group flex items-center py-3 active">
-                    <span className="mr-4 h-px w-8 bg-black transition-all group-hover:w-16 group-hover:bg-gray-500 group-focus-visible:w-16 group-focus-visible:bg-black motion-reduce:transition-none"></span>
-                    <span className="text-xs font-bold uppercase tracking-widest text-gray-500 group-hover:text-black group-focus-visible:text-gray-500">About</span>
-                </Link>
-            </li>
-            <li>
-                <Link href='#experience' className="group flex items-center py-3 active">
-                    <span className="mr-4 h-px w-8 bg-black transition-all group-hover:w-16 group-hover:bg-gray-500 group-focus-visible:w-16 group-focus-visible:bg-black motion-reduce:transition-none"></span>
-                    <span className="text-xs font-bold uppercase tracking-widest text-gray-500 group-hover:text-black group-focus-visible:text-gray-500">Experience</span>
-                </Link>
-            </li>
-            <li>
-                <Link href='#projects' className="group flex items-center py-3 active">
-                    <span className="mr-4 h-px w-8 bg-black transition-all group-hover:w-16 group-hover:bg-gray-500 group-focus-visible:w-16 group-focus-visible:bg-black motion-reduce:transition-none"></span>
-                    <span className="text-xs font-bold uppercase tracking-widest text-gray-500 group-hover:text-black group-focus-visible:text-gray-500">Projects</span>
-                </Link>
-            </li>
-        </ul>
-        </nav>
-  )
+     const container = {
+    hidden: {opacity: 0},
+    show: {
+        opacity: 1,
+        transition: {
+        staggerChildren: 0.2,
+        delayChildren: 0.2,
+        ease: 'easeOut',
+        }
+    }
+    }
+
+    const item = {
+        hidden: { opacity: 0, y: 40 },
+        show: { 
+        opacity: 1, 
+        y: 0,
+        transition: {
+        duration: 0.6,
+        ease: 'easeOut',
+        } },
+    }
+
+    return (
+        <nav className="hidden lg:block">
+            <motion.ul
+            className="mt-16 w-max"
+            variants={container}
+            initial='hidden'
+            whileInView='show'
+            viewport={{once: true, amount:0.2}}>
+                <motion.li
+                variants={item}>
+                    <Link href='#about' className="group flex items-center py-3 active">
+                        <span className="mr-4 h-px w-8 bg-[#3B2D21] transition-all group-hover:w-16 group-hover:bg-red-800 group-focus-visible:w-16 group-focus-visible:bg-red-800 motion-reduce:transition-none"></span>
+                        <span className="text-xs font-bold uppercase tracking-widest group-hover:text-red-800 group-focus-visible:text-red-800 font-garamond ">About</span>
+                    </Link>
+                </motion.li>
+                <motion.li
+                    variants={item}>
+                    <Link href='#experience' className="group flex items-center py-3 active">
+                        <span className="mr-4 h-px w-8 bg-[#3B2D21] transition-all group-hover:w-16 group-hover:bg-red-800 group-focus-visible:w-16 group-focus-visible:bg-red-800 motion-reduce:transition-none"></span>
+                        <span className="text-xs font-bold uppercase tracking-widest group-hover:text-red-800 group-focus-visible:text-red-800 font-garamond ">Experience</span>
+                    </Link>
+                </motion.li>
+                <motion.li
+                    variants={item}>
+                    <Link href='#projects' className="group flex items-center py-3 active">
+                        <span className="mr-4 h-px w-8 bg-[#3B2D21] transition-all group-hover:w-16 group-hover:bg-red-800 group-focus-visible:w-16 group-focus-visible:bg-red-800 motion-reduce:transition-none"></span>
+                        <span className="text-xs font-bold uppercase tracking-widest group-hover:text-red-800 group-focus-visible:text-red-800 font-garamond ">Projects</span>
+                    </Link>
+                </motion.li>
+            </motion.ul>
+            </nav>
+    )
 }
 
 export default Nav

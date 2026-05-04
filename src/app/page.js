f4831f9f84c4e643d7e3461dc78a6aafb4db4c0e
aggicreative555 <142678571+aggicreative555@users.noreply.github.com>
2026-05-04 14:00:24 +0200

    style(assets): add assets
    added images full and blur, added icons, added svgs

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import ProjectsSection from "@/components/ProjectsSection";
 import HandwritingAnimation from "@/components/HandwritingAnimation";
 import FadeInText from "@/components/FadeInText";
 import SvgSection from "@/components/SvgSection";
+import SvgStack from "@/components/SvgStack";
 
 export const metadata = {
   metadataBase: new URL("https://aggi-cieplak.com"),
@@ -24,21 +25,25 @@ export default function Home() {
         <div className="container mx-auto">
           <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-16 lg:py-8">
             <div className="lg:flex lg:justify-between lg:gap-4">
-              <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
+              <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:py-24 lg:pr-30">
                 <div className="flex flex-col gap-4 font-garamond cursor-default">
                   <HandwritingAnimation/>
-                  <FadeInText as='h2' delay={0.2} className='mt-3 text-lg tracking-wider sm:text-xl uppercase' >
+                  <SvgStack/>
+                  <FadeInText as='h2' delay={0.2} className='mt-8 text-lg tracking-wider sm:text-xl uppercase' >
                     JUNIOR FRONTEND DEV
                   </FadeInText>
                   <FadeInText as='p' delay={0.4} className='mt-4 max-w-xs' >
-                    I build creative, practical and structured experiences for the web. 
+                    I build creative, practical and structured experiences for the web. My biggest interest is building story-rich and accessible UI's with impeccably clean code.
                   </FadeInText>
                 </div>
-                <Nav/>
-                <SvgSection/>
+                <div className="flex flex-col justify-between h-full my-14 mb-2">
+                  <Nav/>
+                  <SvgSection/>
+
+                </div>
               </header>
               <main className="pt-24 lg:w-[52%] lg:py-24">
-                <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="About me" id='about'>
+                <section className="mb-16 scroll-mt-16 md:mb-10 lg:mb-20 lg:scroll-mt-24" aria-label="About me" id='about'>
                   <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                     <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only font-garamond">
                         About me
@@ -59,6 +64,16 @@ export default function Home() {
                     </FadeInText>
                   </div>
                 </section>
+                <section className="font-garamond mb-16 scroll-mt-16 md:mb-20 lg:mb-30 lg:scroll-mt-24" aria-label="School projects" id='projects'>
+                  <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                    <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
+                        Projects
+                    </h2>
+                  </div>
+                  <div>
+                    <ProjectsSection/> 
+                  </div>
+                </section>
                 <section className="font-garamond mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Work experience" id='experience'>
                   <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                     <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
@@ -67,16 +82,6 @@ export default function Home() {
                   </div>
                   <div>
                     <ExperienceSection/>
-                  </div>
-                </section>
-                <section className="font-garamond mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="School projects" id='projects'>
-                  <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-                    <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
-                        Projects
-                    </h2>
-                  </div>
-                  <div>
-                    <ProjectsSection/> 
                   </div>
                 </section>
                 <footer className="font-garamond max-w-md pb-16 text-sm text-amber-950/50 sm:pb-0 cursor-default">

468f11adc320cdffff8f6f5566d2bc514af255c8
a g g i <142678571+aggicreative555@users.noreply.github.com>
2026-08-25 09:28:45 +0200

    fix(path): normalise paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/Projects/SwanCom',
+        destination: '/Projects/swancom',
+        permanent: true,
+      },
+      {
+        source: '/Projects/Swancom',
+        destination: '/Projects/swancom',
+        permanent: true,
+      },
+      {
+        source: '/Projects/swanCom',
+        destination: '/Projects/swancom',
+        permanent: true,
+      },
+      {
+        source: '/Projects/SWANCOM',
+        destination: '/Projects/swancom',
+        permanent: true,
+      },
+      {
+        source: '/Projects/SwanCom/:path*',
+        destination: '/Projects/swancom/:path*',
+        permanent: true,
+      },
+      {
+        source: '/Projects/Swancom/:path*',
+        destination: '/Projects/swancom/:path*',
+        permanent: true,
+      },
+      {
+        source: '/Projects/swanCom/:path*',
+        destination: '/Projects/swancom/:path*',
+        permanent: true,
+      },
+      {
+        source: '/Projects/SWANCOM/:path*',
+        destination: '/Projects/swancom/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
